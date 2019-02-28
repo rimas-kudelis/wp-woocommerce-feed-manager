80206b3513fd0559822f9aecc7b418fa20cdd5c3
rextheme <rextheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-02-28 10:23:12 +0000

    tagging version 2.2.3.2
    git-svn-id: https://plugins.svn.wordpress.org/best-woocommerce-feed/trunk@2041331 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rex-product-feed.php
+++ rex-product-feed.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WooCommerce Product Feed Manager
  * Plugin URI:        https://rextheme.com
  * Description:       WooCommerce Product Feed Manager helps you to sell more by uploading products to Google merchant, Amazon, Ebay, Nextag, Pricegrabber and acquiring real buyer.
- * Version:           2.2.3.1
+ * Version:           2.2.3.2
  * Author:            RexTheme
  * Author URI:        https://rextheme.com
  * License:           GPL-2.0+
